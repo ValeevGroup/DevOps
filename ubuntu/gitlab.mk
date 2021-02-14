50388195a1d8a3fdd2eb8b0dc33ae1c21845e9bd
Andrey Asadchev <asadchev@gmail.com>
2021-02-14 13:29:00 -0500

    Ubuntu GitLab ubuntu/gitlab.mk

---
+++ ubuntu/gitlab.mk
@@ -0,0 +1,41 @@
+CMAKE_BUILD_PARALLEL_LEVEL ?= 1
+CUDA_VISIBLE_DEVICES ?= ""
+GITLAB_CONCURRENT ?= 1
+
+gitlab-runner-register = gitlab-runner register \
+  --url https://gitlab.com \
+  --locked=false
+
+gitlab-runner/purge:
+	sudo apt purge gitlab-runner
+	sudo rm -fr /etc/gitlab-runner/config.toml
+	sudo deluser --remove-home gitlab-runner
+
+gitlab-runner/install: /usr/bin/curl
+	sudo bash -o pipefail -c \
+          'curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | bash'
+	sudo apt-get update
+	export GITLAB_RUNNER_DISABLE_SKEL=true; sudo -E apt-get install gitlab-runner
+	sudo usermod -a -G docker gitlab-runner
+	echo "Setting 'concurrent = ${GITLAB_CONCURRENT}' in /etc/gitlab-runner/config.toml"
+	sudo sed 's/^concurrent = .*$\/concurrent = ${GITLAB_CONCURRENT}/' -i /etc/gitlab-runner/config.toml
+
+gitlab-runner/register/shell:
+	sudo ${gitlab-runner-register} \
+   --executor shell \
+   --tag-list shell,valeevgroup
+
+gitlab-runner/register/docker:
+	sudo ${gitlab-runner-register} \
+  --env CMAKE_BUILD_PARALLEL_LEVEL=${CMAKE_BUILD_PARALLEL_LEVEL} \
+  --env CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES} \
+  --docker-runtime nvidia --env NVIDIA_VISIBLE_DEVICES=all \ # --gpus not available yet
+  --executor docker \
+  --tag-list docker,valeevgroup \
+  --docker-pull-policy always \
+  --docker-image valeevgroup/ubuntu
+	echo "Make sure docker.io is installed: make install/docker"
+	echo "Make sure nvidia-docker is installed: make install/nvidia-docker"
+
+gitlab-runner/unregister/all:
+	sudo gitlab-runner unregister --all-runners
